c07ab602e8e7df75a040781dec98d5f9fef7041e
João Victor Prado <joaovictorred@hotmail.com>
2023-01-13 18:24:31 -0300

    setting up the index with logo, title and a description

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from "./styles"
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="imagem de um átomo e react avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="um desenvolvedor de frente para uma tela com linhas"
+    />
+  </S.Wrapper>
 )
 
 export default Main
