aae47a159909235e84df5de6d2480569b68460a0
João Victor Prado <joaovictorred@hotmail.com>
2023-01-13 18:30:04 -0300

    test jest to index and styles

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe("<main/>", () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it("should render colors correctly", () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" })
+  })
 })
