bf69531f4f94c612b05d92e3c65947d40b7ed7f3
João Victor Prado <joaovictorred@hotmail.com>
2023-01-19 21:17:44 -0300

    config to storybook 6.0, config stories to work with globalstyles and add knobs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from "./styles"
 
-const Main = () => (
+const Main = ({
+  title = "React Avançado",
+  description = "TypeScript, ReactJS, NextJS e Styled components"
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="imagem de um átomo e react avançado escrito ao lado."
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="um desenvolvedor de frente para uma tela com linhas"
